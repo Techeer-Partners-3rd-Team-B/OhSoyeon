bcbb686c8fa8dfe138e3364042b288b91cb3b040
ohsoyeon <redosy@naver.com>
2023-09-18 00:37:53 +0900

    message

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="container">
+        <div className="item">
+        <h1>PROFILE</h1>
+        <img src="https://github.com/soyeonXTukorea/2023/blob/main/profile.png?raw=true"></img>
+        <h2>오소연</h2>
+        <h4>birth &nbsp; 2001.06.23</h4>
+        <h4>email &nbsp; redosy@naver.com</h4>
+        <h4>mbti &nbsp; ESFP</h4>
+        </div>
+        <div className="item2">
+          <h1>EDUCATION</h1>
+          <h4>2019.12 &nbsp; &nbsp; 가림고등학교 졸업</h4>
+          <h4>2020.03  &nbsp; &nbsp; 한국공학대학교 입학</h4>
+          <h1>WORK</h1>
+          <h4>2021  &nbsp; &nbsp; 경영학부 학생회 홍보차장</h4>
+          <h4>2022  &nbsp; &nbsp; 경영학부 학생회 홍보부장</h4>
+          <h4>2023  &nbsp; &nbsp; 현장실습서포터즈 드림온</h4>
+          <h1>EXPERIENCE</h1>
+          <h4>2022 &nbsp; &nbsp; 코멘토 부트캠프 참여</h4>
+          <h4>2022 &nbsp; &nbsp; GTQ 1급 취득</h4>
+          <h4>2023 &nbsp; &nbsp; 컴퓨터활용능력 2급 취득</h4>
+        </div>
+      </div>
+  );
+}
+
+export default App;
