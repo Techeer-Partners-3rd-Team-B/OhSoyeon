32179c3d128ff8df977824a86ce7fd343697f8c2
ohsoyeon <redosy@naver.com>
2023-09-20 21:39:34 +0900

    final_profile

---
+++ src/component/Detail.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Detail(){
+    return (
+        <div>
+            <h1>EDUCATION</h1>
+            <h4>2019.12 &nbsp; &nbsp; 가림고등학교 졸업</h4>
+            <h4>2020.03  &nbsp; &nbsp; 한국공학대학교 입학</h4>
+            
+            <h1>WORK</h1>
+            <h4>2021  &nbsp; &nbsp; 경영학부 학생회 홍보차장</h4>
+            <h4>2022  &nbsp; &nbsp; 경영학부 학생회 홍보부장</h4>
+            <h4>2023  &nbsp; &nbsp; 현장실습서포터즈 드림온</h4>
+            
+            <h1>EXPERIENCE</h1>
+            <h4>2022 &nbsp; &nbsp; 코멘토 부트캠프 참여</h4>
+            <h4>2022 &nbsp; &nbsp; GTQ 1급 취득</h4>
+            <h4>2023 &nbsp; &nbsp; 컴퓨터활용능력 2급 취득</h4>
+        </div>
+    )
+}
---
+++ src/component/Link.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Link(){
+    return (
+        <div>
+            <a href="https://www.instagram.com/___soyeonii/">
+            <img src="instagram.png"></img></a>
+            <a href="https://github.com/soyeonXTukorea">
+            <img src="github1.png"></img></a>
+            <a href="https://blog.naver.com/redosy">
+            <img src="blog.png"></img></a>
+        </div>
+    )
+}
